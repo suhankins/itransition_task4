12680f6f105ba8336440486df135821e9050e84e
suhankins <punishedbernadetta@niuitmo.ru>
2023-01-23 03:26:29 +0400

    option to use connection string

--- api/db/indexDb.ts
+++ api/db/indexDb.ts
@@ -2,6 +2,8 @@ import { Client } from 'pg';
 
 export let client: Client;
 export function connectClient() {
-    client = new Client();
+    client = new Client({
+        connectionString: process.env.CONNECTION_STRING
+    });
     client.connect();
 }
