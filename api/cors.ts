87230e69c8d354bcd0abf13107a8781b310d0fd5
suhankins <punishedbernadetta@niuitmo.ru>
2023-01-23 03:26:43 +0400

    commented out cors

--- api/cors.ts
+++ api/cors.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Response, Request } from 'express';
 
 export function cors(_req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
+    //Uncomment if needed
+    //res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Access-Control-Allow-Methods', '*');
+    //res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 }
